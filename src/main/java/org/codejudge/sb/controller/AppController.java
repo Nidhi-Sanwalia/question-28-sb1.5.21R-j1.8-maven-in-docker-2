7c8fbaade9ecd2e253d48fb38d575c3cbab86b31
Nidhi-Sanwalia <nidhisw95@gmail.com>
2020-09-07 02:07:38 +0530

    First Commit

--- src/main/java/org/codejudge/sb/controller/AppController.java
+++ src/main/java/org/codejudge/sb/controller/AppController.java
@@ -1,13 +1,25 @@
 package org.codejudge.sb.controller;
 
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.codejudge.sb.model.Add;
+import org.codejudge.sb.model.Addition;
+import org.codejudge.sb.services.AppService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class AppController {
+
+    @Autowired
+    AppService appService;
+
+    @PostMapping("/api/add")
+    public Addition giveSummation(@RequestBody Add add){
+
+        return appService.findSum(add.getNumber1(),add.getNumber2());
+
+    }
 
     @ApiOperation("This is the hello world api")
     @GetMapping("/")
